3654c86f26f8362322f1c1be1dbe06f486b5235e
Kasparidi <kasparidi@gmail.com>
2020-10-31 11:11:49 +0300

    added ProductRepositoryTest

--- src/main/java/ru/netology/domain/TShirt.java
+++ src/main/java/ru/netology/domain/TShirt.java
@@ -1,5 +1,12 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TShirt extends Product{
     private String color;
     private String size;
--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -1,4 +1,20 @@
 package ru.netology.manager;
 
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
 public class ProductManager {
+    private ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(Product item) {
+        repository.save(item);
+    }
+
+    public void removeById(int id) {
+        repository.removeById(id);
+    }
 }
